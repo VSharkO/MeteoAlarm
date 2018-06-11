8f4298a5235cf1364224b4613fab3bdac19c8adb
VSharkO <valentin.saric@hotmail.com>
2018-06-11 23:59:14 +0200

    added kelvin to celsius conversion

--- Middleware_v2.0.4.exe/Middleware_v2.0.4.exe.ino
+++ Middleware_v2.0.4.exe/Middleware_v2.0.4.exe.ino
@@ -1,12 +1,16 @@
 #include <Ethernet.h>
 #include <SPI.h>
+#include <LiquidCrystal.h>
 
-
+//LCD
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+//service
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 byte ip[] = { 192, 168, 0, 26 };
 byte server[] = { 192, 168, 0, 16 };
-String responce;
-int i=0;
+//variables
+String response;
 String temperature;
 String clouds;
 String windSpeed;
@@ -16,6 +20,9 @@ EthernetClient client;
 
 void setup()
 {
+  //setup lcd num of columns and rows
+  lcd.begin(16, 2);
+  
   Ethernet.begin(mac, ip);
   Serial.begin(9600);
   
@@ -39,7 +46,7 @@ void loop()
   if (client.available()) {
     
     char c = client.read();
-    responce.concat(c);
+    response.concat(c);
     Serial.print(c);
     
     }
@@ -47,12 +54,12 @@ void loop()
     
       if (!client.connected()) {
         Serial.println();
-        printResponceOnLCD();
+        printResponseOnLCD();
 
         
         Serial.println("disconnecting.");
         client.stop();
-        responce = "";
+        response = "";
         delay(60000);
         
         if (client.connect(server, 80)) {
@@ -67,8 +74,22 @@ void loop()
   }
  }
 
-void printResponceOnLCD(){
-    temperature = responce.substring(424,432);
+void printResponseOnLCD(){
+  
+    temperature =response.substring(425,432);
+    clouds = "Clouds: " + response.substring(454,463);
+    windSpeed = "Wind: " + response.substring(484,488)+"mps";
+    pressure = "Pressure: " + response.substring(509,516)+"hPa";
+    float temperatureFloat = temperature.toFloat();
+    if (temperatureFloat>10000){
+        temperatureFloat=((temperatureFloat+272.15f)/100)-272.15f;
+    }
+
+    temperature = "Temp: " + String(temperatureFloat)+ "°C";
     Serial.println(temperature);
+    Serial.println(clouds);
+    Serial.println(windSpeed);
+    Serial.println(pressure);
+    lcd.print(temperature+"\n"+pressure);
   }
 
